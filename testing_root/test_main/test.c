25101e0874fe8a204b3602edaa77469e1edafcfc
SugaSugaRyun <rlarkfbs@gmail.com>
2024-01-24 17:04:45 +0900

    makefile

--- testing_root/test_main/test.c
+++ testing_root/test_main/test.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-//#include "../pushpush_server.c"
+#ifdef TEST
+#else
+#include "../pushpush_server.c"
+#endif
 
 int main(){
 	printf("Happy happy happy\n");
+	write_byte(2, "HI\n", 3);
 	return 0;
 }
